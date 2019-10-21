f688ac79a799564e9093f3ff9c128e27134aa223
mauro.sarti <mauro.sarti.9292@gmail.com>
2019-10-21 11:05:14 -0300

    minnor koin module fix
    Tested in: Samsung Galaxy S8
    Signed off by: Mauro Sarti mauro.sarti@globant.com

--- app/src/main/java/com/globant/di/KoinModules.kt
+++ app/src/main/java/com/globant/di/KoinModules.kt
@@ -1,6 +1,5 @@
 package com.globant.di
 
-import com.globant.domain.usecases.GetCharacterByIdUseCase
 import com.globant.viewmodels.CharacterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,9 +7,3 @@ import org.koin.dsl.module
 val viewModelsModule = module {
     viewModel { CharacterViewModel(get()) }
 }
-
-val useCasesModule = module {
-    single { GetCharacterByIdUseCase() }
-}
-
-
--- di/src/main/java/com/globant/KoinModules.kt
+++ di/src/main/java/com/globant/KoinModules.kt
@@ -4,10 +4,16 @@ import com.globant.data.database.CharacterDatabase
 import com.globant.data.repositories.MarvelCharacterRepositoryImpl
 import com.globant.data.service.CharacterService
 import com.globant.domain.repositories.MarvelCharacterRepository
+import com.globant.domain.usecases.GetCharacterByIdUseCase
 import org.koin.dsl.module
 
 val repositoriesModule = module {
     single { CharacterService() }
     single { CharacterDatabase() }
     single<MarvelCharacterRepository> { MarvelCharacterRepositoryImpl(get(), get()) }
+}
+
+
+val useCasesModule = module {
+    single { GetCharacterByIdUseCase() }
 }
